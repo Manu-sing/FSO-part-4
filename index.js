5be46572e8c4fe6077766616ea6ced6b221c318a
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2022-03-15 10:33:02 +0800

    .gitignored the node_modules file and created the heroku app

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post('/api/blogs', (request, response) => {
     response.json(blog)
   })
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`)
+  })
