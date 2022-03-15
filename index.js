2013394db99c973c6d8a17c55ef35d8dbb1e318e
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2022-03-15 11:34:57 +0800

    connected my back-end to the mongoDB

--- index.js
+++ index.js
@@ -2,54 +2,82 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
 morgan.token('body', (request) => JSON.stringify(request.body))
+const url =
+  `mongodb+srv://Manu-sing:Manu831gallo@cluster0.ru6ma.mongodb.net/blog-list?retryWrites=true&w=majority`
+
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.static('build'))
 
-let blogs = [
-    {
-        id: 1,
-        title: "Alexis Ohanian on his plan to back more emerging managers",
-        author: "Connie Loizos",
-        url: "https://techcrunch.com/2022/03/13/alexis-ohanian-on-his-plan-to-back-more-emerging-managers/",
-        likes: 55,
-        status: "Read"
-      },
-      {
-        id: 2,
-        title: "Augmented reality’s half-decade of stagnation",
-        author: "Lucas Matney",
-        url: "https://techcrunch.com/2022/03/13/augmented-realitys-half-decade-of-stagnation/",
-        likes: 53,
-        status: "Non Read"
-      },
-      {
-        id: 3,
-        title: "Founders: Connect with influential movers and shippers at TC Sessions: Mobility 2022",
-        author: "Alexandra Ames",
-        url: "https://techcrunch.com/2022/03/13/founders-connect-with-influential-movers-and-shippers-at-tc-sessions-mobility-2022/",
-        likes: 54,
-        status: "Read"
-      },
-      {
-        id: 5,
-        title: "Tiger’s stamp of approval is coming for the early stage",
-        author: "Natasha Mascarenhas",
-        url: "https://techcrunch.com/2022/03/12/tiger-global-early-stage/",
-        likes: "45",
-        status: "Read"
-      }
-]
+mongoose.connect(url)
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: String,
+  status: String
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+
+// let blogs = [
+//     {
+//         id: 1,
+//         title: "Alexis Ohanian on his plan to back more emerging managers",
+//         author: "Connie Loizos",
+//         url: "https://techcrunch.com/2022/03/13/alexis-ohanian-on-his-plan-to-back-more-emerging-managers/",
+//         likes: 55,
+//         status: "Read"
+//       },
+//       {
+//         id: 2,
+//         title: "Augmented reality’s half-decade of stagnation",
+//         author: "Lucas Matney",
+//         url: "https://techcrunch.com/2022/03/13/augmented-realitys-half-decade-of-stagnation/",
+//         likes: 53,
+//         status: "Non Read"
+//       },
+//       {
+//         id: 3,
+//         title: "Founders: Connect with influential movers and shippers at TC Sessions: Mobility 2022",
+//         author: "Alexandra Ames",
+//         url: "https://techcrunch.com/2022/03/13/founders-connect-with-influential-movers-and-shippers-at-tc-sessions-mobility-2022/",
+//         likes: 54,
+//         status: "Read"
+//       },
+//       {
+//         id: 5,
+//         title: "Tiger’s stamp of approval is coming for the early stage",
+//         author: "Natasha Mascarenhas",
+//         url: "https://techcrunch.com/2022/03/12/tiger-global-early-stage/",
+//         likes: "45",
+//         status: "Read"
+//       }
+// ]
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
 app.get('/api/blogs', (request, response) => {
-    response.json(blogs)
-})
+    Blog.find({}).then(blogs => {
+      response.json(blogs)
+    })
+  })
 
 app.get('/api/blogs/:id', (request, response) => {
     const id = Number(request.params.id)
